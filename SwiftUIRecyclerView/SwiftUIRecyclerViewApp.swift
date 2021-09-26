1c6a20bd0907b5ce9d9520058c9d88be5b5efbc8
JuYeon Lee <cavss1118@gmail.com>
2021-09-26 13:52:38 +0900

    Initial Commit

---
+++ SwiftUIRecyclerView/SwiftUIRecyclerViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRecyclerViewApp.swift
+//  SwiftUIRecyclerView
+//
+//  Created by 이주연 on 2021/09/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRecyclerViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
