c21ce2d41e59019dbe3a84106662a7dae11b9689
JuYeon Lee <cavss1118@gmail.com>
2021-09-26 14:10:42 +0900

    #2 How to use Recyclerview

--- SwiftUIRecyclerView/ContentView.swift
+++ SwiftUIRecyclerView/ContentView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    let exampleList = [1,2,3,4]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // Example use
+        RecyclerView(
+            setAxis: .HORIZONTAL,
+            setShowBar: true,
+            setSpacing: 0,
+            setContent: {
+                ForEach(1...100, id: \.self) { count in
+                    /*@START_MENU_TOKEN@*/Text("Placeholder \(count)")/*@END_MENU_TOKEN@*/
+                }
+            }
+        )
     }
 }
 
